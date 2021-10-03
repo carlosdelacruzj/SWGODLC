349c4feb79a08c531ff74c57eee9368b272d9a73
Your Name <delacruzcarlos1405@gmail.com>
2021-10-03 00:10:37 -0500

    cambios

--- src/app/control-panel/gestionar-proyecto/service/proyecto.service.ts
+++ src/app/control-panel/gestionar-proyecto/service/proyecto.service.ts
@@ -8,9 +8,14 @@ import { Proyecto } from '../model/proyecto.model';
 })
 export class ProyectoService {
   selectProyecto: Proyecto = {
-    ID: 0,
-    name: '',
-    age: 0,
+    PK_Pro_Cod: 0,
+    Pro_Nombre: '',
+    FK_P_Cod: 0,
+    EPro_Fecha_Inicio_Edicion: '',
+    Pro_Fecha_Fin_Edicion: '',
+    Pro_Revision_Edicion: 0,
+    Pro_Revision_Multimedia: 0,
+    Pro_Enlace: ''
   };
 
   private API_PRUEBA =

a29bd38bb3345fca61a21b6119b88d6e45479018
Your Name <delacruzcarlos1405@gmail.com>
2021-10-05 20:07:03 -0500

    commit prueba carlos

--- src/app/control-panel/gestionar-proyecto/model/proyecto.model.ts
+++ src/app/control-panel/gestionar-proyecto/model/proyecto.model.ts
@@ -1,18 +1,6 @@
 
 
 export class Proyecto {
-<<<<<<< HEAD
-  constructor(
-    public PK_Pro_Cod: number,
-    public Pro_Nombre: string,
-    public FK_P_Cod: number,
-    public EPro_Fecha_Inicio_Edicion: string,
-    public Pro_Fecha_Fin_Edicion: string,
-    public Pro_Revision_Edicion: number,
-    public Pro_Revision_Multimedia: number,
-    public Pro_Enlace: string
-  ) {}
-=======
   constructor(public PK_Pro_Cod: number,
     public Pro_Nombre: string,
     public FK_P_Cod: number,
@@ -22,5 +10,4 @@ export class Proyecto {
     public Pro_Revision_Multimedia: number,
     public Pro_Enlace: null | string
   ) { }
->>>>>>> 70e2325463e5ceb9cb296821ce99a926f4ae53bb
 }

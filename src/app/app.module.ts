db188855618484ac4ca7cdf6fc4b0d1781024d3d
KatyaVergaray <katyavergaray@gmail.com>
2021-10-17 21:31:22 -0500

    paquetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import {
   NgxMatNativeDateModule,
   NgxMatTimepickerModule
 } from '@angular-material-components/datetime-picker';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AdministrarPaqueteServicioComponent } from './control-panel/administrar-paquete-servicio/administrar-paquete-servicio.component';
@@ -60,11 +59,7 @@ import { GestionarPedidoComponent } from './control-panel/gestionar-pedido/gesti
     NgxMatDatetimePickerModule,
     NgxMatTimepickerModule,
     NgxMatNativeDateModule,
-<<<<<<< HEAD
     ReactiveFormsModule, 
-=======
-    ReactiveFormsModule,
->>>>>>> 49dc3f63fc36c2aec236ebbbfc78bf7ee00259f0
     NgbModule
   ],
   providers: [
